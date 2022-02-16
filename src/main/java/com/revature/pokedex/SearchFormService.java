b9ca1adbc364c33a6d09c6b4744b7d15e5e16394
yung-thane <ronald.cristobal.lemus@outlook.com>
2022-02-16 00:49:21 -0500

    Fourth Commit, Added Search Function

--- src/main/java/com/revature/pokedex/SearchFormService.java
+++ src/main/java/com/revature/pokedex/SearchFormService.java
@@ -20,6 +20,7 @@ public class SearchFormService extends HttpServlet {
                 "        <input type='name' name='searchName'/>\n" +
                 "        <input type='submit' value='Search'/>\n" +
                 "    " +
+                "       <a href='/pokemon>'See Full Pokedex</a>\n" +
                 "</form>\n" +
                 "</Body>\n" +
                 "</Html>";
